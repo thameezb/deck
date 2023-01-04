0479289365035c1c1c9a5d625badb3b36a561802
Benjamin Igbeka <benigbeka@double.cloud>
2023-01-04 15:17:50 +0100

    add rules to ignore

--- terraform/modules/bucket/v1/bucket.tf
+++ terraform/modules/bucket/v1/bucket.tf
@@ -47,3 +47,12 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
     prevent_destroy = true
   }
 }
+
+# Enables public access block on the bucket
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket                  = aws_s3_bucket.this.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
