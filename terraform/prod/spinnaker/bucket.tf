4f5991f10dbb781a3e5e669730a8f042c7f23773
Thameez Bodhanya <thameezbo@double.cloud>
2023-03-27 07:04:32 +0000

    Merged in ORION-2473-enforce-spinaker-s3-to-be-private (pull request #13)
    ORION-2473: enforce public-access-block for spinnaker S3 and TF state
    
    * ORION-2473: enforce public-access-block for spinnaker S3 and TF state
    
    
    Approved-by: Salvatore Mazzarino
    Approved-by: Maria Kotliarevskaia

--- terraform/prod/spinnaker/bucket.tf
+++ terraform/prod/spinnaker/bucket.tf
@@ -5,4 +5,8 @@ module "bucket" {
     "arn:aws:iam::022615369514:user/spinnaker",
     "arn:aws:iam::022615369514:root",
   ]
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
